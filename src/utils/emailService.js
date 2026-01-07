7ee44dacec82d4224001ef000854f28fad4f66d5
nouman-naveed <nouman.naveed@hotmail.com>
2026-01-07 10:49:27 +0500

    change email

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.sendgrid.net",
   port: 587,
+  secure: false, // REQUIRED for 587
   auth: {
     user: "apikey",
     pass: process.env.SENDGRID_API_KEY,
